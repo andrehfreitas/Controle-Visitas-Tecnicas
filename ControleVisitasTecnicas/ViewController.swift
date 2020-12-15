c17499c97a77e16c0c5ed3c7e904557697ecc8d6
André Henrique de Freitas <andrehfreitas@gmail.com>
2020-12-14 22:20:55 -0300

    Initial Commit

---
+++ ControleVisitasTecnicas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ControleVisitasTecnicas
+//
+//  Created by Andre Henrique de Freitas on 14/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
